8f98247c406d663ed87ce3ab8fb39a19c885ff37
Ingo Albrecht <prom@berlin.ccc.de>
2012-04-20 17:32:29 +0200

    Added support for daemonization.

--- worldaction.js
+++ worldaction.js
@@ -1,5 +1,8 @@
 var http = require('http'),
-      io = require('socket.io');
+      io = require('socket.io'),
+  daemon = require('daemon');
+
+var opts = require('tav').set();
 
 var WorldAction = function() {
   var that    = {},
@@ -52,17 +55,104 @@ var server = http.createServer(function(req, res){
   res.end("404");
 });
 
-server.listen(8090);
+var listenPort = opts["port"];
+if(!listenPort) {
+    listenPort = 9414;
+    console.log('Defaulting port to ' + listenPort);
+}
 
-var socket       = io.listen(server),
+var listenAddress = opts["address"];
+if(!listenAddress) {
+    listenAddress = '127.0.0.1';
+    console.log('Defaulting address to ' + listenAddress);
+}
+
+var pidFile = opts["pid"];
+if(!pidFile) {
+    pidFile = "worldmap.pid";
+    console.log('Defaulting pid file to ' + pidFile);
+}
+
+var logFile = opts["log"];
+if(!logFile) {
+    logFile = "worldmap.log";
+    console.log('Defaulting log file to ' + logFile);
+}
+
+var daemonize = false;
+
+function banner() {
+    console.log(">>>>>>>>>> Hoccer Worldmap <<<<<<<<<<");
+    console.log("Server running at http://" + listenAddress + ":" + listenPort + "/");
+}
+
+function serve() {
+    server.listen(listenPort, listenAddress);
+
+    var socket       = io.listen(server),
     worldActions = WorldAction();
 
-socket.on('connection', function(client){
-  // new client is here!
-  worldActions.addClient(client);
+    socket.on('connection', function(client){
+                  // new client is here!
+                  worldActions.addClient(client);
+                  
+                  client.on('disconnect', function(){
+                                console.log("disconnected");
+                                worldActions.removeClient(client);
+                            });
+              });
+}
 
-  client.on('disconnect', function(){
-    console.log("disconnected");
-    worldActions.removeClient(client);
-  });
-});
+function start() {
+
+    console.log("Attempting to start worldmap");
+
+    if(daemonize) {
+        var fileDescriptors = {
+            stdout: logFile, stderr: logFile
+        };
+        function daemonizeDone(err, started) {
+            if(err) {
+                console.log("Error starting daemon: " + err);
+                return;
+            }
+
+            banner();
+            serve();
+        }
+        daemon.daemonize(fileDescriptors, pidFile, daemonizeDone);
+    } else {
+        banner();
+        serve();
+    }
+}
+
+function stop() {
+
+    console.log("Attempting to stop worldmap");
+
+    function killDone(err, pid) {
+        if(err) {
+            console.log("Error stopping daemon with pid " + pid + ": " + err);
+        } else {
+            console.log("Stopped daemon with pid " + pid);
+        }
+    }
+
+    daemon.kill(pidFile, killDone);
+}
+
+switch(opts.args[0]) {
+default:
+    daemonize = false;
+    start();
+    break;
+case "start":
+    daemonize = true;
+    start();
+    break;
+case "stop":
+    daemonize = true;
+    stop();
+    break;
+}
